88eab4d3ab149a7452f0ecf085b347fca31f6948
Codling <1300076415@qq.com>
2023-05-25 22:13:35 +0800

    解决路径错误导致的vercel无法部署

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 // 把components中的所组件都进行全局化注册
 // 通过插件的方式
-import ImageView from './imageView/index.vue'
+import ImageView from '@/components/imageView/index.vue'
 import Sku from './XtxSku/index.vue'
 export const componentPlugin = {
     install(app) {
