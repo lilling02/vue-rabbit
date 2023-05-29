a8b12a7430bb165d9054ea31e637c37c74810bfa
Codling <1300076415@qq.com>
2023-05-29 21:51:29 +0800

    登录__请求拦截器--发请求携带token

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { ElMessage } from 'element-plus';
 import 'element-plus/theme-chalk/el-message.css'
 import { useRouter } from 'vue-router'
+import { useUserStore } from '@/stores/user.js'
 // 根据目前是开发环境还是生产环境来配置不同的baseURL
 const baseURL = import.meta.env.VITE_API_BASE_URL;
 
@@ -15,6 +16,13 @@ const request = axios.create({
 // 请求拦截器
 request.interceptors.request.use(config => {
     // 在发送请求之前做些什么
+
+    // TODO : 如果有token,就把token放到请求头中(请求头的key是Authorization)这里我们选择的方式是通过pinia管理数据 通过pinia获取token
+    const { userInfo } = useUserStore();
+    if (userInfo.token) {
+        config.headers.Authorization = 'Bearer ' + userInfo.token;
+        console.log(config.headers.Authorization);
+    }
     return config;
 }
     , error => {
